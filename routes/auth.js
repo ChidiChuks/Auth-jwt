073800610caa9e44b0e5916f9f775ce3dd2bf703
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-08-26 16:16:55 +0100

    installing bcrypt dependency and hashing users password

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,29 @@
 const router = require('express').Router();
 const User = require('../models/User');
-
-//VALIDATION
-const Joi = require('@hapi/joi');
-
-const schema = {
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-};
+const bcrypt = require('bcryptjs');
+const { registerValidation } = require('../validation');
 
 router.post('/register', async(req, res) => {
 
     //LETS VALIDATE THE DATA BEFORE CREATING A USER
-    const { error } = Joi.validate(req.body, schema);
+    const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    //Checking if user is already in the database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('Email already exists');
+
+    //HASH The PASSWORD
+    const salt = await bcypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
-    //Saving the user and adding an async call
+    //Saving the user and adding an async call to catch error
     try {
         const savedUser = await user.save();
         res.send(savedUser);
